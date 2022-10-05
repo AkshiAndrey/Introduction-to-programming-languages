ad0c6237eb35c0089127a8650982e636e79723f9
Andrey Kylkov <tensesamsu@gmail.com>
2022-10-05 16:05:47 +0900

    seminar2

---
+++ seminar2/Program.cs
@@ -0,0 +1,37 @@
+﻿
+/*
+void Sort(int[] array)
+{
+    for j = 0; j <  
+}*/
+
+//int[] array = {1, 2, 3, 4};
+
+//int a = new Random().Next() % 11 * 2;
+//Console.Write(a);
+
+
+/*
+int rand = Random.Next(10, 100);
+
+Console.Write(rand);
+
+int a = rand / 10;
+int b = rand % 10;
+
+if (a > b) Console.Write(a);
+else if (a == b) Console.Write($"{a} = {b}");
+else Console.Write(b);
+
+*/
+
+int rand = Random.Next(100, 1000);
+
+Console.Write(rand);
+
+int a = rand / 10;
+int b = rand % 10;
+
+if (a > b) Console.Write(a);
+else if (a == b) Console.Write($"{a} = {b}");
+else Console.Write(b);
